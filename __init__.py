e43d1b45f244812824659d37a84f15f5fc381458
Pepijn de Vos <pepijndevos@gmail.com>
2021-12-01 17:17:37 +0100

    add simserver API

---
+++ __init__.py
@@ -0,0 +1,89 @@
+from os import name
+import capnp
+from pyttoresque.simserver.Simulator_capnp import Ngspice, Xyce, Cxxrtl
+from bokeh.models import ColumnDataSource
+from collections import namedtuple
+import numpy as np
+
+
+def connect(host, port=5923, simulator=Ngspice):
+    """
+    Connect to a simulation server at the given `host:port`,
+    which is should be a `simulator` such as `Ngspice` or `Xyce`.
+    """
+    return capnp.TwoPartyClient(f"{host}:{port}").bootstrap().cast_as(simulator)
+
+
+def loadFiles(sim, *names):
+    """
+    Load the specified filenames into the simulation server.
+    The first file is the entrypoint for the simulator.
+    Returns a handle to run simulation commands on.
+
+    For in-memory data, directly call `sim.loadFiles`.
+    The data should be of the form `[{"name": name, "content": content"}]`
+    """
+    data = []
+    for name in names:
+        with open(name, 'rb') as f:
+            data.append({
+                "name": name,
+                "contents": f.read()
+            })
+    return sim.loadFiles(data)
+
+
+def map_complex(vec):
+    return np.array(complex(v.real, v.imag) for v in vec.complex)
+
+
+Result = namedtuple("Result", ("scale", "data"))
+
+
+def read(response):
+    """
+    Read one chunk from a simulation command
+    """
+    while True:
+        res = response.result.read().wait()
+        if not res.scale:
+            continue
+        data = {}
+        for vec in res.data:
+                arr = getattr(vec.data, vec.data.which())
+                if arr:
+                    if vec.data.which() == 'complex':
+                        data[vec.name] = map_complex(arr)
+                    else:
+                        data[vec.name] = np.array(arr)
+
+        if data:
+            return Result(res.scale, ColumnDataSource(data))
+        if not res.more:
+            return
+
+
+def stream(response, cds):
+    """
+    Stream simulation data into a ColumnDataSource
+    """
+    while True:
+        res = read(response)
+        if res:
+            cds.stream(res.data.data)
+        else:
+            break
+
+
+def readAll(response):
+    """
+    Read all the simulation data from a simulation command.
+    """
+    res = read(response)
+    while True:
+        newres = read(response)
+        if newres:
+            res.data.stream(newres.data.data)
+        else:
+            break
+    return res
