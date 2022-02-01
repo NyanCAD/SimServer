d85c25b5462ce353327493696853bfe839ec190a
Pepijn de Vos <pepijndevos@gmail.com>
2022-02-01 15:29:35 +0100

    use boost for socket fork stuff

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
-#define NOMINMAX
+#include <stdio.h>     // printf
 
-#include <stdio.h>     // perror, printf
-#include <stdlib.h>    // exit, atoi
-#include <string.h>    // memset
+#include <string> 
 
 #include "api/Simulator.capnp.h"
 #include <kj/debug.h>
@@ -14,21 +12,9 @@
 
 #include "../simserver.h"
 
-#if defined WIN32
-#include <winsock.h>
-typedef int socklen_t;
-#else
-#define closesocket close
-typedef int SOCKET;
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-
-#include <signal.h>
-#include <sys/wait.h>
-#endif
-
-# pragma comment(lib,"ws2_32.lib") //Winsock Library
+#include <boost/asio.hpp>
+#include <boost/process.hpp>
+#include <boost/process/handles.hpp>
 
 class SimulatorImpl final : public Sim::Simulator<SimCommands>::Server
 {
@@ -56,95 +42,48 @@ public:
 
     const kj::Directory &dir;
 };
-#if !defined WIN32
-void handler(int sig) {
-    int ret;
-    wait(&ret);
+
+using boost::this_process::native_handle_type;
+
+int runchild(native_handle_type fd) {
+    kj::AsyncIoContext ctx = kj::setupAsyncIo();
+    auto stream = ctx.lowLevelProvider->wrapSocketFd(fd);
+    auto network = capnp::TwoPartyVatNetwork(*stream, capnp::rpc::twoparty::Side::SERVER);
+    kj::Own<kj::Filesystem> fs = kj::newDiskFilesystem();
+    const kj::Directory &dir = fs->getCurrent();
+    auto rpc = capnp::makeRpcServer(network, kj::heap<SimulatorImpl>(dir));
+    network.onDisconnect().wait(ctx.waitScope);
+    printf("Client disconnected\n");
+    return 0;
 }
-#endif
+
+using namespace boost::asio;
+using ip::tcp;
 
 int main(int argc, char const *argv[])
 {
-    #if defined WIN32
-	WSADATA wsaData;
-	int iResult = WSAStartup(MAKEWORD(2 ,2), &wsaData);
-	if (iResult != 0) {
-		printf("error at WSASturtup\n");
-		return 0;
-	}
-    #else
-    signal(SIGCHLD, handler);
-    #endif
-
-    SOCKET serverFd, clientFd;
-    struct sockaddr_in server, client;
-    socklen_t len;
     int port = 5923;
-    char buffer[1024];
     if (argc == 2 && strcmp(argv[1], "--help")==0) {
         printf("%s [port]\n", argv[0]);
         return 0;
+    } else if (argc == 3 && strcmp(argv[1], "--child")==0) {
+        native_handle_type fd = std::stoi(argv[2]);
+        return runchild(fd);
     } else if (argc == 2)
     {
-        port = atoi(argv[1]);
+        port = std::stoi(argv[1]);
     }
-    serverFd = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverFd < 0)
-    {
-        perror("Cannot create socket");
-        exit(1);
-    }
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(port);
-    len = sizeof(server);
-    if (bind(serverFd, (struct sockaddr *)&server, len) < 0)
-    {
-        perror("Cannot bind sokcet");
-        exit(2);
-    }
-    if (listen(serverFd, 10) < 0)
-    {
-        perror("Listen error");
-        exit(3);
-    }
-    printf("listening on port %d\n", port);
+    boost::asio::io_service io_service;
+    tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v6(), port));
     while (1)
     {
-        len = sizeof(client);
         printf("waiting for clients\n");
-        if ((clientFd = accept(serverFd, (struct sockaddr *)&client, &len)) < 0)
-        {
-            perror("accept error");
-            exit(4);
-        }
-        #if defined WIN32
-        int pid = 0; // yolo no forking for you
-        #else
-        pid_t pid = fork();
-        #endif
-        if (pid < 0) {
-            perror("fork error");
-            exit(4);
-        } else if (pid == 0) {
-            char *client_ip = inet_ntoa(client.sin_addr);
-            printf("Accepted new connection from a client %s:%d\n", client_ip, ntohs(client.sin_port));
-            kj::AsyncIoContext ctx = kj::setupAsyncIo();
-            auto stream = ctx.lowLevelProvider->wrapSocketFd(clientFd);
-            auto network = capnp::TwoPartyVatNetwork(*stream, capnp::rpc::twoparty::Side::SERVER);
-            kj::Own<kj::Filesystem> fs = kj::newDiskFilesystem();
-            const kj::Directory &dir = fs->getCurrent();
-            auto rpc = capnp::makeRpcServer(network, kj::heap<SimulatorImpl>(dir));
-            network.onDisconnect().wait(ctx.waitScope);
-            printf("Client disconnected: %s:%d\n", client_ip, ntohs(client.sin_port));
-            #if !defined WIN32
-            return 0;
-            #endif
-        }
+        tcp::socket peersocket(io_service);
+        acceptor.accept(peersocket);
+        auto endpoint = peersocket.remote_endpoint();
+        printf("Accepted new connection from a client %s:%d\n", endpoint.address(), endpoint.port());
+        std::string fd = std::to_string(peersocket.native_handle());
+        boost::process::spawn(argv[0], "--child", fd);
     }
-    closesocket(serverFd);
-    #if defined WIN32
-	WSACleanup();
-    #endif
     return 0;
 }
